411158db2e1ba0376d53543e9aaed6c53e57d181
Kryštof Rainisch <stofi@users.noreply.github.com>
2022-05-04 23:22:28 +0200

    bruh 2

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,11 @@
-import "./entry.css";
-import "./style.css";
+import './entry.css'
+import './style.css'
 
-console.log("hi main");
+console.log('hi main')
 // import Experience from "./Experience/Experience";
 
 // const canvas = document.querySelector("canvas.webgl") as HTMLCanvasElement;
 
 // new Experience(canvas);
 
-const rootDiv = document.querySelector<HTMLDivElement>("#root");
-
-if (rootDiv) {
-  rootDiv.innerText = "this is a webpack playground with typescript and 3js";
-}
-console.log("hi xxx");
+console.log('hi xxx')
--- webpack/webpack.base.conf.js
+++ webpack/webpack.base.conf.js
@@ -1,60 +1,67 @@
-const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 function resolve(dir) {
-  return path.join(__dirname, "..", dir);
+    return path.join(__dirname, '..', dir)
 }
 
 module.exports = {
-  context: resolve("./"),
-  entry: {
-    home: resolve("./src/main.ts")
-  },
-  output: {
-    filename: "static/[name].[contenthash].js",
-    path: resolve("dist")
-  },
-  module: {
-    rules: [
-      // TS
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/
-      },
+    context: resolve('./'),
+    entry: {
+        home: resolve('./src/main.ts')
+    },
+    output: {
+        filename: 'static/[name].[contenthash].js',
+        path: resolve('dist')
+    },
+    module: {
+        rules: [
+            // TS
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
 
-      // JS
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"]
-      },
+            // JS
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
 
-      // CSS
-      {
-        test: /\.css$/,
-        use: ["css-loader", "postcss-loader"]
-      },
+            // CSS
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader'
+                ]
+            },
 
-      // Shaders
-      {
-        test: /\.(glsl|frag|vert)$/,
-        exclude: /node_modules/,
-        use: ["raw-loader"]
-      }
+            // Shaders
+            {
+                test: /\.(glsl|frag|vert)$/,
+                exclude: /node_modules/,
+                use: ['raw-loader']
+            }
+        ]
+    },
+    // resolve: {
+    //   src: resolve("./src")
+    // },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'webpack playground',
+            filename: 'index.html',
+            template: 'src/index.html',
+            chunks: 'all'
+        }),
+        new MiniCssExtractPlugin()
     ]
-  },
-  // resolve: {
-  //   src: resolve("./src")
-  // },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: "webpack playground",
-      filename: "index.html",
-      template: "src/index.html",
-      chunks: "all"
-    })
-  ]
-};
+}
